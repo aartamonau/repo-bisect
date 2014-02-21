d7bca14a1727214d46fa9488fcaa87e8f2b38a0e
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2014-02-20 21:14:24 -0800

    Can find repo root directory.

--- src/RepoSnapshot.hs
+++ src/RepoSnapshot.hs
@@ -1,2 +1,23 @@
+import System.Directory (getCurrentDirectory, canonicalizePath,
+                         doesDirectoryExist)
+import System.FilePath (combine, takeDirectory)
+
+repoDirName :: FilePath
+repoDirName = ".repo"
+
+findRootDir :: IO (Maybe FilePath)
+findRootDir = go =<< canonicalizePath =<< getCurrentDirectory
+  where go :: FilePath -> IO (Maybe FilePath)
+        go dir = do
+          exists <- doesDirectoryExist $ combine dir repoDirName
+          if exists
+            then return $ Just dir
+            else maybe (return Nothing) go (parent dir)
+
+        parent :: FilePath -> Maybe FilePath
+        parent dir | dir /= parentDir = Just parentDir
+                   | otherwise        = Nothing
+          where parentDir = takeDirectory dir
+
 main :: IO ()
-main = putStrLn "doing nothing so far"
+main = print =<< findRootDir
