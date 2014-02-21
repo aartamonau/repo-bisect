7538d8ced0e305200cb9fafab7d726b6dd447d40
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2014-02-20 21:25:34 -0800

    Don't use Maybe. Just fail with error.

--- src/RepoSnapshot.hs
+++ src/RepoSnapshot.hs
@@ -1,3 +1,5 @@
+import Control.Monad (when)
+
 import System.Directory (getCurrentDirectory, canonicalizePath,
                          doesDirectoryExist)
 import System.FilePath (combine, takeDirectory)
@@ -5,19 +7,18 @@ import System.FilePath (combine, takeDirectory)
 repoDirName :: FilePath
 repoDirName = ".repo"
 
-findRootDir :: IO (Maybe FilePath)
+findRootDir :: IO FilePath
 findRootDir = go =<< canonicalizePath =<< getCurrentDirectory
-  where go :: FilePath -> IO (Maybe FilePath)
+  where go :: FilePath -> IO FilePath
         go dir = do
           exists <- doesDirectoryExist $ combine dir repoDirName
           if exists
-            then return $ Just dir
-            else maybe (return Nothing) go (parent dir)
-
-        parent :: FilePath -> Maybe FilePath
-        parent dir | dir /= parentDir = Just parentDir
-                   | otherwise        = Nothing
-          where parentDir = takeDirectory dir
+            then return dir
+            else do
+              let parentDir = takeDirectory dir
+              when (parentDir == dir) $
+                error "Could not find .repo directory"
+              go parentDir
 
 main :: IO ()
 main = print =<< findRootDir
